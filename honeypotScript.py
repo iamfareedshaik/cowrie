e5b1a753f1514e5dab98107ccd3e803f392b85c8
iamfareedshaik <143288633+iamfareedshaik@users.noreply.github.com>
2024-06-12 04:58:18 -0500

    Update honeypotScript.py

--- honeypotScript.py
+++ honeypotScript.py
@@ -1,40 +1,41 @@
+import json
 import time
 import os
-import requests
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+import http.client
+import pyinotify
 
 # Configuration
 LOG_FILE_PATH = '/home/cowrie/cowrie/var/log/cowrie/cowrie.json'
-API_ENDPOINT = 'http://16.171.142.151:3000/item'
+API_HOST = '16.171.142.151'
+API_PORT = 3000
+API_ENDPOINT = '/item'
 
-class LogFileHandler(FileSystemEventHandler):
-    def __init__(self, file_path):
+class LogFileHandler(pyinotify.ProcessEvent):
+    def my_init(self, file_path):
         self.file_path = file_path
-        self.file = open(self.file_path, 'r')
-        self.file.seek(0, 2)  # Move the cursor to the end of the file
-        print("initialized from honeypot -------------------")
 
-    def on_modified(self, event):
-        if event.src_path == self.file_path:
+    def process_IN_MODIFY(self, event):
+        if event.pathname == self.file_path:
             self.send_new_lines()
 
     def send_new_lines(self):
-        while True:
-            line = self.file.readline()
-            if not line:
-                break
-            self.send_to_api(line.strip())
+        with open(self.file_path, 'r') as file:
+            for line in file:
+                self.send_to_api(line.strip())
 
     def send_to_api(self, log_data):
         print("---------------------------------")
         print(log_data)
         print("--------------------------------")
-        response = requests.post(API_ENDPOINT, json={'log': log_data})
-        if response.status_code == 200:
+        connection = http.client.HTTPConnection(API_HOST, API_PORT)
+        headers = {'Content-type': 'application/json'}
+        log_data = {'log': log_data}
+        connection.request('POST', API_ENDPOINT, headers=headers, body=json.dumps(log_data))
+        response = connection.getresponse()
+        if response.status == 200:
             print(f"Successfully sent log: {log_data}")
         else:
-            print(f"Failed to send log: {log_data}, Status Code: {response.status_code}")
+            print(f"Failed to send log: {log_data}, Status Code: {response.status}")
 
 if __name__ == "__main__":
     # Wait for the log file to be created
@@ -43,15 +44,14 @@ if __name__ == "__main__":
         print(f"Waiting for {LOG_FILE_PATH} to be created...")
         time.sleep(30)
 
-    event_handler = LogFileHandler(LOG_FILE_PATH)
-    observer = Observer()
-    observer.schedule(event_handler, path=LOG_FILE_PATH, recursive=False)
-    observer.start()
+    wm = pyinotify.WatchManager()
+    handler = LogFileHandler(file_path=LOG_FILE_PATH)
+    notifier = pyinotify.Notifier(wm, default_proc_fun=handler)
+    wm.add_watch(LOG_FILE_PATH, pyinotify.IN_MODIFY)
+
     print(f"Monitoring {LOG_FILE_PATH} for changes...")
 
     try:
-        while True:
-            time.sleep(1)
+        notifier.loop()
     except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+        notifier.stop()
