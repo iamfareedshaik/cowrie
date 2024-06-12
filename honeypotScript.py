5a0c2d493d5d8d7944d9d900647f968f77146678
iamfareedshaik <143288633+iamfareedshaik@users.noreply.github.com>
2024-06-12 02:58:03 -0500

    Update honeypotScript.py

--- honeypotScript.py
+++ honeypotScript.py
@@ -1,17 +1,19 @@
 import time
+import os
 import requests
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 # Configuration
-LOG_FILE_PATH = '/var/log/cowrie/cowrie.json'  # Path to the log file
-API_ENDPOINT = 'http://16.171.142.151:3000/item'  # API endpoint to send log data
+LOG_FILE_PATH = '/home/cowrie/cowrie/var/log/cowrie/cowrie.json'
+API_ENDPOINT = 'http://16.171.142.151:3000/item'
 
 class LogFileHandler(FileSystemEventHandler):
     def __init__(self, file_path):
         self.file_path = file_path
         self.file = open(self.file_path, 'r')
         self.file.seek(0, 2)  # Move the cursor to the end of the file
+        print("initialized from honeypot -------------------")
 
     def on_modified(self, event):
         if event.src_path == self.file_path:
@@ -25,7 +27,9 @@ class LogFileHandler(FileSystemEventHandler):
             self.send_to_api(line.strip())
 
     def send_to_api(self, log_data):
-      print(log_data)
+        print("---------------------------------")
+        print(log_data)
+        print("--------------------------------")
         response = requests.post(API_ENDPOINT, json={'log': log_data})
         if response.status_code == 200:
             print(f"Successfully sent log: {log_data}")
@@ -33,6 +37,12 @@ class LogFileHandler(FileSystemEventHandler):
             print(f"Failed to send log: {log_data}, Status Code: {response.status_code}")
 
 if __name__ == "__main__":
+    # Wait for the log file to be created
+    print("welcome")
+    while not os.path.exists(LOG_FILE_PATH):
+        print(f"Waiting for {LOG_FILE_PATH} to be created...")
+        time.sleep(30)
+
     event_handler = LogFileHandler(LOG_FILE_PATH)
     observer = Observer()
     observer.schedule(event_handler, path=LOG_FILE_PATH, recursive=False)
